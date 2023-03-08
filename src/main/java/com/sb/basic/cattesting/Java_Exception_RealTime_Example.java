0338450cec8468b71260b606da5fc6aa269f05ca
selvanayaki91 <selvarjunan91@gmail.com>
2023-03-08 17:37:30 -0500

    2nd commit

--- src/main/java/com/sb/basic/cattesting/Java_Exception_RealTime_Example.java
+++ src/main/java/com/sb/basic/cattesting/Java_Exception_RealTime_Example.java
@@ -5,6 +5,8 @@ public class Java_Exception_RealTime_Example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		
+		System.out.println("Test");
 
 	}
 
